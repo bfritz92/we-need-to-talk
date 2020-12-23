30d2b5c924960753baa5b9cb82ddf7617c7bff8c
bfritz92 <67482120+bfritz92@users.noreply.github.com>
2020-12-23 11:14:13 -0500

    change podcast logo

--- category-podcast.php
+++ category-podcast.php
@@ -10,8 +10,8 @@ get_header(); ?>
 		<div class="grid-x grid-margin-x grid-padding-x contained">
 			<div class="cell small-12 large-6 podcast--img">
 			<picture class="about-page--section--img" alt="">
-				<source srcset="<?php echo wp_get_attachment_image_srcset( 2921) ?>">
-					<img src="<?php echo wp_get_attachment_image_url( 2921, 'large' ) ?>">
+				<source srcset="<?php echo wp_get_attachment_image_srcset( 9) ?>">
+					<img src="<?php echo wp_get_attachment_image_url( 9, 'large' ) ?>">
 			</picture>
 			</div>
 			<div class="cell small-12 large-6 limit-900">
